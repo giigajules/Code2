88b43bd833c35cf5695b7d929cc564a8f3802eed
giigajules <giigajules@gmail.com>
2025-10-27 11:30:22 +0100

    made pizza with canvas

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -4,6 +4,8 @@ var Canvas;
     document.addEventListener("DOMContentLoaded", hndLoad);
     let canvas;
     let crc2;
+    let radius;
+    let numPieces;
     function hndLoad() {
         canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
@@ -11,17 +13,34 @@ var Canvas;
         canvas.width = 1520;
         crc2.fillStyle = "#65e49eff";
         crc2.fillRect(0, 0, canvas.width, canvas.height);
-        drawTriangle();
+        userInput();
+        drawTriangleCircle(radius, numPieces);
     }
-    function drawTriangle() {
+    function drawTriangle(_centerPoint, _firstVec, _secondVec) {
         const triangle = new Path2D();
-        triangle.moveTo(100, 50);
-        triangle.lineTo(50, 150);
-        triangle.lineTo(150, 150);
+        triangle.moveTo(_centerPoint.x, _centerPoint.y);
+        triangle.lineTo(_firstVec.x, _firstVec.y);
+        triangle.lineTo(_secondVec.x, _secondVec.y);
         triangle.closePath();
         crc2.fillStyle = "#e64e6cff";
         crc2.fill(triangle);
         crc2.strokeStyle = "#000000";
         crc2.stroke(triangle);
+    }
+    function userInput() {
+        radius = parseInt(prompt("Enter the radius of the triangle-circle: ", "1") || "1", 10);
+        numPieces = parseInt(prompt("Enter its number of pieces: ", "1") || "5", 10);
+    }
+    function drawTriangleCircle(_radius, _numPieces) {
+        const centerPoint = { x: 700, y: 400 };
+        for (let i = 0; i < _numPieces; i++) {
+            const x1 = Math.sin(Math.PI * 2 / _numPieces * i) * _radius + centerPoint.x;
+            const y1 = Math.cos(Math.PI * 2 / _numPieces * i) * _radius + centerPoint.y;
+            const x2 = Math.sin(Math.PI * 2 / _numPieces * (i + 1)) * _radius + centerPoint.x;
+            const y2 = Math.cos(Math.PI * 2 / _numPieces * (i + 1)) * _radius + centerPoint.y;
+            const firstVec = { x: x1, y: y1 };
+            const secondVec = { x: x2, y: y2 };
+            drawTriangle(centerPoint, firstVec, secondVec);
+        }
     }
 })(Canvas || (Canvas = {}));
--- Canvas/canvas.ts
+++ Canvas/canvas.ts
@@ -3,6 +3,8 @@ namespace Canvas {
 
     let canvas: HTMLCanvasElement;
     let crc2: CanvasRenderingContext2D;
+    let radius: number;
+    let numPieces: number;
 
     type Vector2 = {
         x: number;
@@ -19,20 +21,38 @@ namespace Canvas {
         crc2.fillStyle = "#65e49eff";
         crc2.fillRect(0, 0, canvas.width, canvas.height);
 
-        drawTriangle();
+        userInput();
+        drawTriangleCircle(radius, numPieces);
     }
 
-    function drawTriangle(): void {
+    function drawTriangle(_centerPoint: Vector2, _firstVec: Vector2, _secondVec: Vector2): void {
         const triangle: Path2D = new Path2D();
-        triangle.moveTo(100, 50);
-        triangle.lineTo(50, 150);
-        triangle.lineTo(150, 150);
+        triangle.moveTo(_centerPoint.x, _centerPoint.y);
+        triangle.lineTo(_firstVec.x, _firstVec.y);
+        triangle.lineTo(_secondVec.x, _secondVec.y);
         triangle.closePath();
-    
+
         crc2.fillStyle = "#e64e6cff";
-        crc2.fill(triangle);        
+        crc2.fill(triangle);
         crc2.strokeStyle = "#000000";
-        crc2.stroke(triangle);  
+        crc2.stroke(triangle);
     }
 
+    function userInput(): void {
+        radius = parseInt(prompt("Enter the radius of the triangle-circle: ", "1") || "1", 10);
+        numPieces = parseInt(prompt("Enter its number of pieces: ", "1") || "5", 10);
+    }
+
+    function drawTriangleCircle(_radius: number, _numPieces: number): void {
+        const centerPoint: Vector2 = {x: 700, y: 400};
+        for (let i: number = 0; i < _numPieces; i++) {
+            const x1: number = Math.sin(Math.PI * 2 / _numPieces * i) * _radius + centerPoint.x;
+            const y1: number = Math.cos(Math.PI * 2 / _numPieces * i) * _radius + centerPoint.y;
+            const x2: number = Math.sin(Math.PI * 2 / _numPieces * (i+1)) * _radius + centerPoint.x;
+            const y2: number = Math.cos(Math.PI * 2 / _numPieces * (i+1)) * _radius + centerPoint.y;
+            const firstVec: Vector2 = {x: x1, y: y1};
+            const secondVec: Vector2 = {x: x2, y: y2};
+            drawTriangle(centerPoint, firstVec, secondVec);
+        }
+    }
 }
